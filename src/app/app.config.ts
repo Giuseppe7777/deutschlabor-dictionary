ff57fc3f36ff1cefc06328b2bac9790772b47635
Yosyp Malanka <malankajosyp@gmail.com>
2026-05-09 23:50:10 +0200

    feat(i18n): add ngx-translate language foundation

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,27 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
+import { provideTranslateService } from '@ngx-translate/core';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { DEFAULT_INTERFACE_LANGUAGE } from './core/i18n/interface-language';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
-  ]
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideHttpClient(),
+
+    provideTranslateService({
+      loader: provideTranslateHttpLoader({
+        prefix: '/i18n/',
+        suffix: '.json',
+      }),
+      fallbackLang: DEFAULT_INTERFACE_LANGUAGE,
+      lang: DEFAULT_INTERFACE_LANGUAGE,
+    }),
+  ],
 };
---
+++ src/app/core/i18n/interface-language.ts
@@ -0,0 +1,23 @@
+export const SUPPORTED_INTERFACE_LANGUAGES = [
+  'de',
+  'en',
+  'uk',
+  'ru',
+  'pl',
+  'hu',
+] as const;
+
+export type InterfaceLanguage = (typeof SUPPORTED_INTERFACE_LANGUAGES)[number];
+
+export const DEFAULT_INTERFACE_LANGUAGE: InterfaceLanguage = 'de';
+
+export const INTERFACE_LANGUAGE_STORAGE_KEY = 'deutschlabor.interfaceLanguage';
+
+export function isSupportedInterfaceLanguage(
+  value: string | null | undefined,
+): value is InterfaceLanguage {
+  return (
+    typeof value === 'string' &&
+    (SUPPORTED_INTERFACE_LANGUAGES as readonly string[]).includes(value)
+  );
+}
